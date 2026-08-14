7982a85ab168095af53fd073c0e80a6efc49476b
dubem4521-dot <dubem4521@gmail.com>
2026-08-14 13:33:59 +0200

    Intial commit - getting a .git file to help with auto deploment

---
+++ deploy.bat
@@ -0,0 +1,12 @@
+@echo off
+echo ===============================
+echo Deploying to nginx
+echo ===============================
+
+set nginx_path=C:\Users\Taku\AppData\Local\Microsoft\WinGet\Packages\nginxinc.nginx_Microsoft.Winget.Source_8wekyb3d8bbwe\nginx-1.31.3\html
+
+echo copying files to %nginx_path%
+copy index.html %nginx_path%\index.html /Y
+copy style.css %nginx_path|style.css /Y 2>nul
+
+echo FILES SUCCESSFULY DEPLOYED
